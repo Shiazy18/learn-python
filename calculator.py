0d6b246b2bb3e65a344246c9019ad5cd0080b7b2
Shivam <shivampant0998@gmail.com>
2025-11-16 14:59:58 +0530

    added lower condiotn for operation

--- calculator.py
+++ calculator.py
@@ -2,13 +2,13 @@ firstNum = input("Enter First number: ")
 secNum = input("Enter Second Number: ")
 operation = input(" 1. Sum \n 2. Subtraction \n 3. Multiplication \n 4. Divide \n Select Operation: ")
 
-if operation == "sum" or operation == "1":
+if operation.lower() == "sum" or operation == "1":
     ans = float(firstNum) + float(secNum)
-elif operation == "subtraction" or operation == "2":
+elif operation.lower() == "subtraction" or operation == "2":
     ans = float(firstNum) - float(secNum) 
-elif operation == "Multiplication" or operation == "3":
+elif operation.lower() == "multiplication" or operation == "3":
     ans = float(firstNum) * float(secNum) 
-elif operation == "Divide" or operation == "4":
+elif operation.lower() == "divide" or operation == "4":
     ans = float(firstNum) / float(secNum) 
 else:
     print("Wrong input. Enter either the operation nane or number associated with that operation")
